84daf752d37d80185feb116766cf028c649da940
Kunal <jainkunal129@gmail.com>
2023-10-07 19:01:06 +0530

    bcrypt implemented

--- src/routes/encryption.js
+++ src/routes/encryption.js
@@ -1,41 +1,18 @@
-const crypto = require('crypto')
-const { SECRET_KEY, SECRET_IV, ECNRYPTION_METHOD } = process.env
+const bcrypt = require('bcrypt');
+const saltRounds = 10; // Number of salt rounds for bcrypt (adjust as needed)
 
-// const { secret_key, secret_iv, ecnryption_method } = encryptionConfig
-const secret_key = SECRET_KEY;
-const secret_iv = SECRET_IV;
-const ecnryption_method = ECNRYPTION_METHOD
 
-if (!secret_key || !secret_iv || !ecnryption_method) {
-    throw new Error('secretKey, secretIV, and ecnryptionMethod are required')
-}
+// Function to hash a password
+const hashPassword = async (password) => {
+    return await bcrypt.hash(password, saltRounds);
+};
 
-// Generate secret hash with crypto to use for encryption
-const key = crypto
-    .createHash('sha512')
-    .update(secret_key)
-    .digest('hex')
-    .substring(0, 32)
-const encryptionIV = crypto
-    .createHash('sha512')
-    .update(secret_iv)
-    .digest('hex')
-    .substring(0, 16)
+// Function to verify a password
+const verifyPassword = async  (password, hashedPassword) => {
+    return await bcrypt.compare(password, hashedPassword);
+};
 
-// Encrypt data
-module.exports.encryptData = (data) => {
-    const cipher = crypto.createCipheriv(ecnryption_method, key, encryptionIV)
-    return Buffer.from(
-        cipher.update(data, 'utf8', 'hex') + cipher.final('hex')
-    ).toString('base64') // Encrypts data and converts to hex and base64
-}
-
-// Decrypt data
-module.exports.decryptData = (encryptedData) => {
-    const buff = Buffer.from(encryptedData, 'base64')
-    const decipher = crypto.createDecipheriv(ecnryption_method, key, encryptionIV)
-    return (
-        decipher.update(buff.toString('utf8'), 'hex', 'utf8') +
-        decipher.final('utf8')
-    ) // Decrypts data and converts to utf8
-}
+module.exports = {
+    hashPassword,
+    verifyPassword,
+};
