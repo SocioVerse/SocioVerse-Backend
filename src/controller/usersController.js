bdc2f250cc55499f1f9ca5031bbf909be865a30c
Kartik0125 <bishtkartik485@gmail.com>
2023-10-08 12:27:32 +0530

    comment is added

--- src/controller/usersController.js
+++ src/controller/usersController.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { signup } = require("../services/usersServices")
 const router = express.Router();
 
-
+//comment
 router.route("/signup").post(signup)
 module.exports = router;
 
