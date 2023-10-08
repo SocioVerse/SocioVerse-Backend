0b395543f6bf71e00881bf2a8b4563d2c5b6f14a
Gagandeep Singh <deepgagan9956@gmail.com>
2023-10-08 23:46:10 +0530

    updated user model

--- src/models/usersModel.js
+++ src/models/usersModel.js
@@ -1,76 +1,90 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     email: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     phone_number: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     password: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     username: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     occupation: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     profile_pic: {
-        type: String,
-        trim: true
+      type: String,
+      trim: true,
     },
     country: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     dob: {
-        type: Date,
-        required: true,
+      type: Date,
+      required: true,
     },
     gender: {
-        type: String,
-        trim: true
-
+      type: String,
+      trim: true,
     },
     website: {
-        type: String,
-        trim: true
-
+      type: String,
+      trim: true,
     },
     face_image_dataset: [
-        {
-            type: String,
-            trim: true,
+      {
+        type: String,
+        trim: true,
 
-            default: [],
-        }
+        default: [],
+      },
     ],
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "userSchema",
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "userSchema",
+      },
+    ],
+    socioThread_bio: {
+      type: String,
+      trim: true,
+    },
+    socioVerse_bio: {
+      type: String,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-
-
-
-
-
-
-}, {
-    timestamps: true
-});
-
-module.exports = mongoose.model('users', userSchema);
+module.exports = mongoose.model("users", userSchema);
