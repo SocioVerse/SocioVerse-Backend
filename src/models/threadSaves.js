3fa8adf61562c43bc48b2a0fb8bcda0510fb0ebf
Gagan2601 <deepgagan9956@gmail.com>
2023-12-23 17:10:43 +0530

    Save Thread Count added and api's corrected

---
+++ src/models/threadSaves.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const savedThreadsSchema = new Schema(
+  {
+    thread_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "threadsSchema",
+      required: true,
+    },
+    saved_by: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "userSchema",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("threadSaves", savedThreadsSchema);
